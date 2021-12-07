923ae5bc4455581b9b230ba0cf36a4db282d90c3
JayaKrishnaNamburu <namburu1995@gmail.com>
2021-12-07 19:11:55 +0530

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'styled-components': require.resolve('./node_modules/styled-components/dist/styled-components.esm.js'),
+    }
+
+    return config
+  },
+  experimental: {
+    styledComponents: true,
+    urlImports: ['https://jscdn.teleporthq.io']
+  }
 }
